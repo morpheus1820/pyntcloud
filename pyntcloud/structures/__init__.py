6ecc1336b3023504a7e54f54a5ac4daa24b1b33b
morpheus1820 <rosa.stefano@gmail.com>
2022-03-22 18:28:23 +0100

    added fixedvoxelgrid structure

--- pyntcloud/structures/__init__.py
+++ pyntcloud/structures/__init__.py
@@ -5,10 +5,12 @@ from .convex_hull import ConvexHull
 from .delaunay import Delaunay3D
 from .kdtree import KDTree
 from .voxelgrid import VoxelGrid
+from .voxelgrid import FixedVoxelGrid
 
 ALL_STRUCTURES = {
     'convex_hull': ConvexHull,
     'delaunay3D': Delaunay3D,
     'kdtree': KDTree,
-    'voxelgrid': VoxelGrid
+    'voxelgrid': VoxelGrid,
+    'fixedvoxelgrid': FixedVoxelGrid
 }
